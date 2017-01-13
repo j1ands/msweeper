decddff3c1ff3eccb6446faf21d271bba511c7e2
jordan landau <jordanlandau@jordans-MacBook.local>
2017-01-12 19:52:23 -0500

    brute force reveal on first click

--- app/scripts/services/boardgenerator.js
+++ app/scripts/services/boardgenerator.js
@@ -69,7 +69,7 @@ angular.module('msweeperApp')
         }
         return false;
       });
-      handleFirstSelection(size, click);
+      handleTileSelection(size, click.target, true);
     }
 
     function checkMine(size, position, option) {
@@ -114,35 +114,63 @@ angular.module('msweeperApp')
       }
     }
 
-    function handleFirstSelection(size, click) {
-      handleTileSelection(size, click.target, true);
-      var square = click.target;
-      var table = click.currentTarget;
-      var rows = $(table).find('tr');
-      var cells = {};
-      angular.forEach(rows, function(cols, row) { cells[row] = cols.children; });
-      revealTiles(square, cells, size);
-    }
+    // function handleFirstSelection(size, click) {
+    //   handleTileSelection(size, click.target, true);
+    //   var square = click.target;
+    //   var table = click.currentTarget;
+    //   var rows = $(table).find('tr');
+    //   var cells = {};
+    //   angular.forEach(rows, function(cols, row) { cells[row] = cols.children; });
+    //   revealTiles(square, cells, size);
+    // }
 
-    function revealTiles(square, cells, size) {
-      //use cells
-      if(square.innerHTML === '0') {
-        if(+square.dataset.col < (size / 2)) {
-          if(+square.dataset.row < (size / 2)) {
-            while(square.innerHTML === '0' && +square.dataset.row < size - 1) {
-              square.dataset.row = (+square.dataset.row + 1).toString();
-              handleTileSelection(size, square, false);
-            }
-            // while
-            while(square.innerHTML !== '0' || square.className !== 'mine' || +square.dataset.col < size - 1) {
-              square.dataset.col = (+square.dataset.col + 1).toString();
-              handleTileSelection(size, square, false);
-            }
+    // function revealTiles(square, cells, size) {
+    //   //use cells
+    //   var newSquare = square;
+    //   var squareVal = square.innerHTML;
+    //   // var difference = square.innerHTML === '0' ? 1 : -1;
+    //   // if(square.innerHTML === '0') {
+    //   if(+newSquare.dataset.col < (size / 2)) {
+    //     if(+newSquare.dataset.row < (size / 2)) {
+    //       while(newSquare.innerHTML === squareVal || +newSquare.dataset.row < size - 1 || newSquare.className !== 'mine') {
+    //         newSquare = cells[+newSquare.dataset.row + 1][newSquare.dataset.col];
+    //         handleTileSelection(size, newSquare, false);
+    //       }
+    //       newSquare.innerHTML = '';
+    //       newSquare.className = '';
+    //       newSquare = cells[+square.dataset.row - 1][square.dataset.col];
 
-          }
-        }
-      }
-    }
+    //       while()
+    //         while(newSquare.innerHTML !== squareVal || +newSquare.dataset.col < size - 1 || newSquare.className !== 'mine') {
+    //           newSquare = cells[newSquare.dataset.row][+newSquare.dataset.col + 1];
+    //           handleTileSelection(size, newSquare, false);
+    //         }
+    //         newSquare.innerHTML = '';
+    //         newSquare.className = '';
+    //         newSquare = cells[newSquare.dataset.row][+newSquare.dataset.col - 1];
+    //         while(newSquare.innerHTML === squareVal || +newSquare.dataset.row > 0 || newSquare.className !== 'mine') {
+    //           newSquare = cells[+newSquare.dataset.row - 1][newSquare.dataset.col];
+    //           handleTileSelection(size, newSquare, false);
+    //         }
+    //         newSquare.innerHTML = '';
+    //         newSquare.className = '';
+    //         newSquare = cells[+square.dataset.row + 1][square.dataset.col];
+    //     // else if row is >= size / 2
+    //     } else {
+
+    //     }
+    //       // while
+    //       // while(square.innerHTML !== '0' || square.className !== 'mine' || +square.dataset.col < size - 1) {
+    //         square.dataset.col = (+square.dataset.col + 1).toString();
+    //         handleTileSelection(size, square, false);
+    //       // }
+
+    //   // else if col is >= size / 2
+    //   } else {
+
+    //   }
+    //   // }
+    // }
 
     window.oncontextmenu = function(click) {
       click.target.className = 'marked';
